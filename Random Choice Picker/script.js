b87b3ae42532eddbf28f5a5d9845cbcbd02ccdf0
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2021-08-27 19:30:57 +0200

    Creating tag elems and populating functionality

--- Random Choice Picker/script.js
+++ Random Choice Picker/script.js
@@ -10,5 +10,13 @@ textarea.addEventListener('keyup', e => {
 const createTags = (input) => {
     const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());  
 
-    console.log(tags)
+    tagsElem.innerHTML = '';
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span');
+        tagEl.classList.add('tag');
+        tagEl.innerText = tag;
+
+        tagsElem.appendChild(tagEl);
+    });
 }
